f45a8de8abd3459cec5a5cfc081be6f357a86a13
arbinzaman <arbinzmn@gmail.com>
2022-08-21 01:03:07 +0600

    added phn Field Functionality

--- js/phn.js
+++ js/phn.js
@@ -1,9 +1,35 @@
-document.getElementById('btn-phn-minus').addEventListener('click',function(){
+
+function upatingPhnField(isIncreaseing){
     const phnField = document.getElementById('phn-field');
     const phnFieldString = phnField.value;
     const PreviousPhnField = parseInt(phnFieldString);
+let newPhnField;
+    if(isIncreaseing === true){
+       newPhnField = PreviousPhnField + 1;
+    }
 
-
-    const newPhnField = PreviousPhnField + 1;
+    else{
+       newPhnField = PreviousPhnField - 1;
+    }
     phnField.value= newPhnField;
+    return newPhnField;
+}
+
+function updateTotalPhnPrice (newPhnField){
+
+    const phnPrice = document.getElementById('phn-price');
+    const newPhnPrice = newPhnField * 1219;
+    phnPrice.innerText= newPhnPrice;
+}
+
+document.getElementById('btn-phn-plus').addEventListener('click',function(){
+    const newPhnField = upatingPhnField(true);
+    updateTotalPhnPrice(newPhnField);
+
+    
+})
+
+document.getElementById('btn-phn-minus').addEventListener('click',function(){
+    const newPhnField = upatingPhnField(false);
+    updateTotalPhnPrice(newPhnField);
 })
